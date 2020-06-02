5f758e9ce93433689d1c4dc4b0dd9959470633e9
jshinde <jshindems@gmail.com>
2020-06-02 14:09:15 -0400

    initial setup

--- nostaldja/models.py
+++ nostaldja/models.py
@@ -4,6 +4,11 @@ from django.db import models
 
 #Define a class for Decades
 #primary key
+class Decade(models.Model):
+    start_year=models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.start_year
 
 
 # Define a class for Fad that inherits from the mode.Model class
@@ -12,4 +17,12 @@ from django.db import models
 # description=TextField()
 # decade= foreign key
 
+class Fad(models.Model):
+    name = models.CharField(max_length=100)
+    image_url = models.TextField()
+    description = models.TextField()
+    decade = models.ForeignKey(Decade, on_delete=models.CASCADE, related_name='fads')
+
+    def __str__(self):
+        return self.name
 #Don't forget when don to make migrations and migrate
